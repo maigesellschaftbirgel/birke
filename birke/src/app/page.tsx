3177e22eabf8c2797d00535d03db86bb3fdaa047
Adralonter <florian.collip@gmail.com>
2025-01-26 18:28:45 +0100

    Setup Next.js

---
+++ birke/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="bg-black p-4 text-white">
+      <Button>Hello World</Button>
+    </div>
+  );
+}
